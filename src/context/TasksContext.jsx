7bb37f89583431111e9042cec8f018522be1e580
KarlBelousov <belousovkarl@gmail.com>
2026-04-30 23:36:23 +0600

    кастомные хуки для работы прложения

--- src/context/TasksContext.jsx
+++ src/context/TasksContext.jsx
@@ -1,96 +1,27 @@
-import {
-  createContext,
-  useState,
-  useEffect,
-  useRef,
-  useCallback,
-  useMemo,
-} from "react";
+import { createContext } from "react";
+import useTasks from "../hooks/useTasks";
+import useIncompleteTaskScroll from "../hooks/useIncompleteTaskScroll";
 
 const TasksContext = createContext({});
 
 const TasksProvider = ({ children }) => {
-  const [tasks, setTasks] = useState(() => {
-    const savedTasks = localStorage.getItem("tasks");
+  const {
+    tasks,
+    filteredTasks,
+    deleteTask,
+    deleteAllTasks,
+    toggleTaskComplete,
+    newTaskTitle,
+    newTaskInputRef,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    addTask,
+  } = useTasks();
 
-    if (savedTasks) {
-      return JSON.parse(savedTasks);
-    }
+  const { firstIncompleteTaskRef, firstIncompleteTaskId } =
+    useIncompleteTaskScroll(tasks);
 
-    return [
-      { id: "task-1", title: "Купить молоко", isDone: false },
-      { id: "task-2", title: "Погладить кота", isDone: true },
-    ];
-  });
-  const [newTaskTitle, setNewTaskTitle] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
-
-  const newTaskInputRef = useRef(null);
-  const firstIncompleteTaskRef = useRef(null);
-  const firstIncompleteTaskId = tasks.find(({ isDone }) => !isDone)?.id;
-
-  const deleteAllTasks = useCallback(() => {
-    const isConfirmed = confirm("Are you sure you want to delete all?");
-
-    if (isConfirmed) {
-      setTasks([]);
-    }
-  }, []);
-
-  const deleteTask = useCallback(
-    (taskId) => {
-      setTasks(tasks.filter((task) => task.id !== taskId));
-    },
-    [tasks],
-  );
-
-  const toggleTaskComplete = useCallback(
-    (taskId, isDone) => {
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === taskId) {
-            return { ...task, isDone };
-          }
-
-          return task;
-        }),
-      );
-    },
-    [tasks],
-  );
-
-  const addTask = useCallback(() => {
-    if (newTaskTitle.trim().length > 0) {
-      const newTask = {
-        id: crypto?.randomUUID() ?? Date.now().toString(),
-        title: newTaskTitle,
-        isDone: false,
-      };
-
-      setTasks((prevTasks) => [...prevTasks, newTask]);
-      setNewTaskTitle("");
-      setSearchQuery("");
-      newTaskInputRef.current.focus();
-    }
-  }, [newTaskTitle]);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
-
-  useEffect(() => {
-    newTaskInputRef.current.focus();
-  }, []);
-
-  const filteredTasks = useMemo(() => {
-    const clearSearchQuery = searchQuery.trim().toLowerCase();
-
-    return clearSearchQuery.length > 0
-      ? tasks.filter(({ title }) =>
-          title.toLowerCase().includes(clearSearchQuery),
-        )
-      : null;
-  }, [searchQuery, tasks]);
   return (
     <TasksContext.Provider
       value={{
